07ccf2018404e6122cafc0dca8a087d03b956389
geisslercom <b.geissler89@googlemail.com>
2015-08-20 15:26:14 +0200

    ecma 6 adjust (arrow) on anon func in example

--- example.js
+++ example.js
@@ -3,19 +3,19 @@ var Susi = require('./susi');
 var susi = new Susi();
 
 // register preprocessor for all events
-susi.registerProcessor('.*', function(evt) {
+susi.registerProcessor('.*', (evt) => {
     evt.payload = evt.payload || {};
     evt.ack();
 });
 
 // add 'foo' processor
-var processorId = susi.registerProcessor('foo', function(evt) {
+var processorId = susi.registerProcessor('foo', (evt) => {
     evt.payload.foo = 'bar';
     evt.ack();
 });
 
 // add 'foo' consumer
-var consumerId = susi.registerConsumer('foo', function(evt) {
+var consumerId = susi.registerConsumer('foo', (evt) => {
     console.log('consumer', JSON.stringify(evt.payload));
 });
 
@@ -24,23 +24,23 @@ var consumerId = susi.registerConsumer('foo', function(evt) {
  * @param {json} processor/cusomer information
  */
 susi.publish({topic : 'foo'})
-    .then(function(evt){
+    .then((evt)=>{
         console.log('finish', JSON.stringify(evt.payload));
         console.log('-----------------------------------');
         susi.unregisterProcessor(processorId);
     })
-    .catch(function (evt , err) {
+    .catch((evt , err)=>  {
         console.log('err');
         console.error(err);
     });
 
-susi.publish({topic : 'foo'}).then(function(evt){
+susi.publish({topic : 'foo'}).then((evt)=>{
     console.log('finish', JSON.stringify(evt.payload));
     console.log('-----------------------------------');
     susi.unregisterConsumer(consumerId);
 });
 
-susi.publish({topic : 'foo'}).then(function(evt){
+susi.publish({topic : 'foo'}).then((evt)=>{
     console.log('---------------Done----------------');
      susi.unregisterConsumer(consumerId);
 });
